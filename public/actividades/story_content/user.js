0525f53095e8317adb9deed9a689e0c80497b0da
flizcano <flizcanor@icloud.com>
2023-04-14 01:03:24 -0500

    Actualiza plantilla

---
+++ public/actividades/story_content/user.js
@@ -0,0 +1,15 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6PconsJ9P5N":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.close();
+}
+
--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -3,15 +3,18 @@
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
 
-    p Con el diagrama de actividades se termina la descripción de las tres técnicas usadas para la especificación y el análisis de requisitos, como se pudo analizar cada una de las técnicas nos brinda características para lograr representar gráficamente y de una forma clara, los requisitos que un usuario necesita poder cumplir satisfactoriamente con las solicitudes de los clientes y usuarios, por otro lado se pueden usar todas las técnicas en una especificación y análisis de requisitos si se ve la necesidad.
-    p.mb-5 A continuación, se muestra un mapa conceptual con los elementos más importantes desarrollados en este componente.
+    p El proceso de interpretación del lenguaje de señas es una labor de gran responsabilidad, por ende, es fundamental conocer las condiciones, protocolos, y las técnicas necesarias para garantizar un servicio de calidad.
+
+    p Por otro lado, es necesario reconocer los lineamientos normativos establecidos dentro del contexto de interpretación y la comunidad sorda esto con el fin de garantizar el cumplimiento de los derechos fundamentales de las personas con discapacidad.
+
+    p.mb-5 Una breve revisión de los temas vistos, se encuentran en el siguiente esquema :
 
     .row.justify-content-center
       .col-lg-10.mb-5
         figure
           img(src="@/assets/curso/sintesis.svg", alt="alt")
       .col-auto
-        a.anexo.mb-4(:href="obtenerLink('/downloads/prueba.pdf')" target="_blank")
+        a.anexo.mb-4(:href="obtenerLink('/downloads/sintesis.pdf')" target="_blank")
           .anexo__icono
             img(src="@/assets/template/icono-pdf.svg")
           .anexo__texto
